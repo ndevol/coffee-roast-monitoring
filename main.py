2f6bc9b41304d3ee6d51f94151cfee565ac78dc2
Nathan DeVol <nsdevol@gmail.com>
2025-11-18 22:53:34 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import board
+import digitalio
+
+import adafruit_max31856
+
+# Create sensor object, communicating over the board's default SPI bus
+spi = board.SPI()
+
+# allocate a CS pin and set the direction
+cs = digitalio.DigitalInOut(board.D5)
+cs.direction = digitalio.Direction.OUTPUT
+
+# create a thermocouple object with the above
+thermocouple = adafruit_max31856.MAX31856(spi, cs)
+
+# print the temperature!
+print(f"{thermocouple.temperature * 9/5 + 32:.2f}°F")
