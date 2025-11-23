bf444bf60b0bc38f256055c9bc586bc04e1e2f93
nsdevol <nsdevol@gmail.com>
2025-11-22 18:09:35 -0800

    Extract functions

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ pi = False
 import collections
 import datetime
 import random
+import logging
 
 import dash
+import dash_daq as daq
 import plotly.graph_objs as go
 
 from dash import dcc, html, callback, Output, Input
@@ -26,7 +28,11 @@ class MockThermocouple:
 # Create Dash app
 app = dash.Dash(__name__)
 app.layout = html.Div([
-    html.H3('Coffee Time!'),
+    daq.BooleanSwitch(
+        on=False,
+        label="Read Temperature Data",
+        labelPosition="top"
+    ),
     dcc.Graph(id='live-update-graph'),
     dcc.Interval(
         id='interval-component',
@@ -41,21 +47,15 @@ app.layout = html.Div([
     Input('interval-component', 'n_intervals'),
 )
 def update_graph_live(_):
-    temp, t = read_temperature()
-    if temp is None:
-        return dash.no_update
-
-    X.append(t)
-    Y.append(temp)
-
-    return create_temperature_plot(X, Y)
+    read_temperature()
+    return create_temperature_plot(temp_plot, time_plot)
 
 
-def initialize_deques(maxlen: int = 60*5) -> tuple[collections.deque, collections.deque]:
-    """Initialize deques for storing time and temperature data"""
-    X = collections.deque(maxlen=maxlen)
-    Y = collections.deque(maxlen=maxlen)
-    return X, Y
+def initialize_plot_deques(maxlen_plot: int = 60*5) -> tuple[collections.deque, collections.deque]:
+    """Initialize deques for storing time and temperature data for plotting."""
+    temp_plot = collections.deque(maxlen=maxlen_plot)
+    time_plot = collections.deque(maxlen=maxlen_plot)
+    return temp_plot, time_plot
 
 
 def initialize_thermocouple():
@@ -69,30 +69,59 @@ def initialize_thermocouple():
     return adafruit_max31856.MAX31856(spi, cs)
 
 
-def read_temperature(fahrenheit: bool = True) -> tuple[float | None, datetime.datetime | None]:
+def read_temperature(fahrenheit: bool = True) -> None:
     """Read temperature from thermocouple."""
     try:
         reading_time = datetime.datetime.now()
-        temp_c = thermocouple.temperature
+        temp = thermocouple.temperature
         if fahrenheit:
-            return temp_c * 9/5 + 32, reading_time
-        return temp_c, reading_time
+            temp = temp * 9/5 + 32, reading_time
+
+        temp_plot.append(temp)
+        time_plot.append(reading_time)
+
+        if recording:
+            record_data(temp, reading_time)
+
+
     except Exception as e:
         print(f"Error reading temperature: {e}")
         return None, None
 
 
-def create_temperature_plot(X, Y, fahrenheit: bool = True, y_padding: float = 5):
+def record_data(temp: float, reading_time: datetime.datetime, maxlen: int = 60*30) -> None:
+    """Record temperature data."""
+    temp_recorded.append(temp)
+    time_recorded.append(reading_time)
+
+    if len(temp_recorded) > maxlen:
+        logging.warning("maxlen reached for recording, writing to database.")
+        write_data_to_db()
+        temp_recorded.clear()
+        time_recorded.clear()
+        # TODO change toggle back
+
+def write_data_to_db():
+    """Write temp_recorded and time_recorded to database."""
+    # Take the first time as the timestamp for the entry
+    ...
+
+
+def create_temperature_plot(temp_plot, time_plot, fahrenheit: bool = True, y_padding: float = 5):
     """Create timeseries temperature plot."""
-    fig = go.Figure(data=[go.Scatter(x=list(X), y=list(Y))])
+    fig = go.Figure(data=[go.Scatter(x=list(temp_plot), y=list(time_plot))])
     fig.update_layout(
         yaxis_title=f"Temperature (°{'F' if fahrenheit else 'C'})",
-        yaxis=dict(range=[min(Y) - y_padding, max(Y) + y_padding] if Y else [0, 100]),
+        # yaxis=dict(
+        #     range=[min(time_plot) - y_padding, max(time_plot) + y_padding] if time_plot else [0, 100]
+        # ),
     )
     return fig
 
 
 if __name__ == '__main__':
-    X, Y = initialize_deques()
+    recording = False
+    temp_recorded, time_recorded = [], []
+    temp_plot, time_plot = initialize_plot_deques()
     thermocouple = initialize_thermocouple()
     app.run(debug=True, host="0.0.0.0")
